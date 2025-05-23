dd70d601e947e2b40857d579d28152813e0aa587
ina2002 <m18862716686@163.com>
2025-05-23 20:15:48 +0800

    1

---
+++ canvas-nest.js
@@ -0,0 +1,6 @@
+(document.getElementById("cas"), (f = document.getElementsByTagName("script"), {
+        zIndex: (y = f[f.length - 1]).getAttribute("zIndex"),
+        opacity: y.getAttribute("opacity"),
+        color: y.getAttribute("color"),
+        count: Number(y.getAttribute("count")) || 99
+    }))
